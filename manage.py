6b2a4a7540a8478a0f082dc2ebc7f9c3e5c092d7
AndrewIndeche <indecheandrew@gmail.com>
2021-09-28 10:30:38 +0300

    error edits

--- manage.py
+++ manage.py
@@ -1,11 +1,11 @@
 from app import create_app,db
-from app.models import User
-from flask_script import Manager
+from app.models import User,Pitch
+from flask_script import Manager, Server
 from  flask_migrate import Migrate, MigrateCommand
 
-app = create_app('production')
-manager = Manager(app)
+app = create_app('prod')
 
+manager = Manager(app)
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 
